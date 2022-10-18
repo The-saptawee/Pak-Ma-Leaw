cd2d1f43e43c7dac4a50f3b42b47db754219284a
kingdrunk <thanakorn22543@hotmail.com>
2022-10-18 13:01:28 +0700

    feat: 🎸 (backup) added windows os base connect psql

--- models/index.js
+++ models/index.js
@@ -1,27 +1,26 @@
 const Sequelize = require("sequelize");
+// const sequelize = new Sequelize(
+//   "postgresql://pak_local:aa010203@localhost:5432/pak_local?sslmode=disable"
+// );
+
 const sequelize = new Sequelize(
-  "postgresql://pak_local:aa010203@localhost:5432/pak_local?sslmode=disable"
+  "pak_local", // นี่เป็นชื่อ DB ของเราน
+  "postgres", // user ที่ใช้สรการเข้าไปยัง db
+  "12345678", // password
+  {
+    host: "localhost", // host ของ db ที่เราสร้างเอาไว้
+    dialect: "postgres", // 'mysql' | 'mariadb' | 'postgres' | 'mssql'   พวกนี้ใช่ก็ใช้ได้นะจ๊ะ
+    define: {
+      timestamps: false, //ส่วนตรงนี้ก็เป็นการตั้งค่าเพิ่มเติม
+    },
+  }
 );
-
-// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-//   host: dbConfig.HOST,
-//   dialect: dbConfig.dialect,
-//   operatorsAliases: false,
-
-//   pool: {
-//     max: dbConfig.pool.max,
-//     min: dbConfig.pool.min,
-//     acquire: dbConfig.pool.acquire,
-//     idle: dbConfig.pool.idle,
-//   },
-// });
 
 const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-// db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
 db.role = require("./role.model.js")(sequelize, Sequelize);
 db.customers = require("./customers.model")(sequelize, Sequelize);
 db.hrs = require("./hrs.model")(sequelize, Sequelize);
@@ -36,5 +35,16 @@ db.materails = require("./materail.model")(sequelize, Sequelize);
 db.assets = require("./assets.model")(sequelize, Sequelize);
 db.typeo = require("./typeo.model")(sequelize, Sequelize);
 
-
 module.exports = db;
+
+// ส่วนนี้เป็นการตั้งต่า relation โดยเป็นการบอกว่าใน 1 team มีได้หลาย player ง่ายๆ ก็คือ relation แบบ 1:M
+//   db.team.hasMany(
+//     db.player,
+//     {
+//         foreignKey: { name: 'tid', field: 'tid' }, //name ตรงสำคัญพยายามตั่งให้เป็นชื่อเดียวกับ FK ใน table ที่นำไปใช้นะครับ
+//     }
+//   };
+
+// ส่วนนี้เป็นการตั้ง relation แบบกลับกันกับด้านบน จริงแล้วเราไม่ตั้งก็ได้แต่แนะนำให้ตั้งเอาไว้ เพราะเวลาที่เราไม่ได้ใส่
+// line นี้จะทำให้เราสามารถใช้  team ในการหา player ได้อย่างเดียวและไม่สามารถใช้ player หา team ได้
+//   db.player.belongsTo(db.team, { foreignKey: 'tid' });
